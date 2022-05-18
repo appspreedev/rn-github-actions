362fe4160c1134d789719f63a2279b682cddbe31
Mihai Chifor <mihai.chifor@mcro-e.com>
2022-05-18 23:14:41 +0300

    workflows(github): cleanup

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -1,8 +1,12 @@
 echo "$RELEASE_KEYSTORE" > release.keystore.asc
-gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch release.keystore.asc > fastlane/release.keystore
+gpg -d --passphrase="$SECRETS_PASSPHRASE" --batch release.keystore.asc > fastlane/release.keystore
+
+echo "$RELEASE_KEYSTORE_INFO" > keystore-info.asc
+gpg -d --passphrase="$SECRETS_PASSPHRASE" --batch keystore-info.json.asc > fastlane/keystore-info.json
 
 #echo "$SERVICE_ACCOUNT" > service_account.json.asc
 #gpg -d --passphrase="$SERVICE_ACCOUNT_PASSPHRASE" --batch service_account.json.asc > android/fastlane/service_account.json
 
 #rm release.keystore.asc service_account.json.asc
-rm release.keystore.asc
+
+rm release.keystore.asc keystore-info.asc
--- .github/scripts/decrypt_ios_keys.sh
+++ .github/scripts/decrypt_ios_keys.sh
@@ -1,4 +1,4 @@
 echo "$APPSTORE_API_KEY" > appstore_key.json.asc
-gpg -d --passphrase="$APPSTORE_API_KEY_PASSPHRASE" --batch appstore_key.json.asc > fastlane/appstore_key.json
+gpg -d --passphrase="$SECRETS_PASSPHRASE" --batch appstore_key.json.asc > fastlane/appstore_key.json
 
 rm appstore_key.json.asc
