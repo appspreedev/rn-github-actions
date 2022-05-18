8969bd2c1d82ebf9c0ca7ecacd8547119f7cf179
Mihai Chifor <mihai.chifor@mcro-e.com>
2022-05-19 00:04:08 +0300

    fix: android build workflow

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -1,12 +1,9 @@
 echo "$RELEASE_KEYSTORE" > release.keystore.asc
 gpg -d --passphrase="$SECRETS_PASSPHRASE" --batch release.keystore.asc > fastlane/release.keystore
 
-echo "$RELEASE_KEYSTORE_INFO" > keystore-info.json.asc
-gpg -d --passphrase="$SECRETS_PASSPHRASE" --batch keystore-info.json.asc > fastlane/keystore-info.json
-
 #echo "$SERVICE_ACCOUNT" > service_account.json.asc
 #gpg -d --passphrase="$SERVICE_ACCOUNT_PASSPHRASE" --batch service_account.json.asc > android/fastlane/service_account.json
 
 #rm release.keystore.asc service_account.json.asc
 
-rm release.keystore.asc keystore-info.json.asc
+rm release.keystore.asc
